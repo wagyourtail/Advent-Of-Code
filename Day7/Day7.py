4f72feb9c7d9d7c6f9d2d3b34b27312d9f7e6393
Wagyourtail <wagyourtail@wagyourtail.xyz>
2021-12-07 00:12:18 -0700

    smarter D7 p2 soln

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -19,13 +19,24 @@ def part2(data, part1_result):
             min_c = cost
     return min_c
 
+def part2_no_brute_force(data):
+    data = [int(x) for x in data]
+    target = statistics.mean(data)
+    floor = sum(sum(range(int(abs(i - int(target)) + 1))) for i in data)
+    ceil = sum(sum(range(int(abs(i - int(target) - 1)) + 1)) for i in data)
+    if floor < ceil:
+        return floor
+    else:
+        return ceil
+
 
 def main():
     with open('inp7.txt', 'r') as f:
         data = f.read().split(",")
 
     print('Part 1: {}'.format(part1_result := part1(data)))
-    print('Part 2: {}'.format(part2(data, part1_result)))
+    print('Part 2 brute_force: {}'.format(part2(data, part1_result)))
+    print('Part 2: {}'.format(part2_no_brute_force(data)))
 
 
 if __name__ == "__main__":
