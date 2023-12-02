901473a86f410d7eae59ebd6f24e9d9ead2c6d7a
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-12-01 23:18:44 -0700

    d2

---
+++ src/day2/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+mod part1;
+mod part2;
+
+pub struct Shared {
+    p1_input: String,
+    p2_input: Option<String>,
+}
+
+fn main() {
+
+    let part1 = fs::read_to_string("input/day2/part1.txt").expect("Error reading file");
+    let part2 = {
+        let metadata = fs::metadata("input/day2/part2.txt");
+        if metadata.is_ok() && metadata.unwrap().is_file() {
+            Some(fs::read_to_string("input/day2/part2.txt").expect("Error reading file"))
+        } else {
+            None
+        }
+    };
+
+    let shared = &mut Shared {
+        p1_input: part1,
+        p2_input: part2,
+    };
+
+    println!("Day 1 - Part 1: {}", part1::solve(shared));
+    println!("Day 1 - Part 2: {}", part2::solve(shared));
+
+}
---
+++ src/day2/part2.rs
@@ -0,0 +1,36 @@
+use std::cmp::max;
+use crate::Shared;
+
+pub fn solve(shared: &mut Shared) -> String {
+    let mut value = 0;
+    shared.p1_input.split('\n').for_each(|line| {
+        let semi = line.chars().into_iter().position(|c| c == ':').unwrap();
+        let key = line.get(5..semi).unwrap().parse::<i32>().unwrap();
+        let mut else_ = true;
+        let mut red_count = 0;
+        let mut blue_count = 0;
+        let mut green_count = 0;
+        for game in line.get(semi+1..line.len()).unwrap().split(';') {
+            let mut red = 0;
+            let mut blue = 0;
+            let mut green = 0;
+            for tile in game.split(',') {
+                let mut t = tile.split(' ');
+                t.next();
+                let number = t.next().unwrap();
+                let color = t.next().unwrap();
+                match color {
+                    "red" => red += number.parse::<i32>().unwrap(),
+                    "blue" => blue += number.parse::<i32>().unwrap(),
+                    "green" => green += number.parse::<i32>().unwrap(),
+                    _ => (),
+                }
+            }
+            red_count = max(red, red_count);
+            blue_count = max(blue, blue_count);
+            green_count = max(green, green_count);
+        }
+        value += red_count * blue_count * green_count;
+    });
+    return value.to_string();
+}
